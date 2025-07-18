b0ee59e055cbbebc8d74a03f3ca4e21d9980457a
Kim-254-de <abt5.09919.23@student.tharaka.ac.ke>
2025-07-18 13:33:15 +0300

    Update changes

--- Evoting/forms.py
+++ Evoting/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Voter
+from datetime import date
+
 
 class VoterRegistrationForm(forms.ModelForm):
     class Meta:
@@ -9,9 +11,21 @@ class VoterRegistrationForm(forms.ModelForm):
             'national_id': forms.TextInput(attrs={'placeholder': 'Registration Number'}),
             'name': forms.TextInput(attrs={'placeholder': 'Full Name'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Email Address'}),
+
             'faculty': forms.Select(attrs={'class': 'form-control'}),
             'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
         }
+    def clean_date_of_birth(self):
+        dob = self.cleaned_data['date_of_birth']
+        today = date.today()
+        age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+
+        if age < 15:
+            raise forms.ValidationError("You must be at least 15 years old to register.")
+        
+        # Optionally, auto-set the age field
+        self.cleaned_data['age'] = age
+        return dob
 
 class VoterVerificationForm(forms.ModelForm):
     class Meta:
