205999e62c58dff29230b70da2098455991fad6c
Kim-254-de <abt5.09919.23@student.tharaka.ac.ke>
2025-07-19 19:25:30 +0300

    Update models

--- Evoting/admin.py
+++ Evoting/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html_join
 from .models import Voter, Candidate, Vote
+from django.utils.html import format_html
 
 class VoterAdmin(admin.ModelAdmin):
     list_display = ('national_id', 'name', 'date_of_birth', 'age', 'department', 'has_voted', 'is_verified', 'registered_by', 'registered_at', 'verified_by', 'verified_at')
@@ -9,8 +10,8 @@ admin.site.register(Voter, VoterAdmin)
 
 class CandidateAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'position', 'party', 'votes', 'voters_list', 'is_faculty_representative', 'faculty')
-    list_filter = ('position', 'party', 'is_faculty_representative', 'faculty')
+    list_display = ('name', 'position', 'party', 'votes', 'voters_list', 'is_faculty_representative', 'faculty', 'display_photo')
+    list_filter = ('position', 'party', 'is_faculty_representative', 'faculty', 'display_photo')
 
     def voters_list(self, obj):
         voters = obj.vote_set.select_related('voter').all()
@@ -20,6 +21,14 @@ class CandidateAdmin(admin.ModelAdmin):
             ((vote.voter.name, vote.voter.national_id) for vote in voters)
         ) if voters else "-"
     voters_list.short_description = "Voters"
+
+    def display_photo(self, obj):
+        if obj.photo:
+            return format_html('<img src="{}" width="50" height="50" style="border-radius: 50%;" />', obj.photo.url)
+
+        return 'No Photo'
+    display_photo.allow_tags = True
+    display_photo.short_description = 'Photo'
 
 admin.site.register(Candidate, CandidateAdmin)
 admin.site.register(Vote)
