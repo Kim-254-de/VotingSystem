3915cebda61afe7ff7ab506fa86af4a04a139ac7
Kim-254-de <abt5.09919.23@student.tharaka.ac.ke>
2025-07-19 12:12:22 +0300

    Add Changes

--- Evoting/urls.py
+++ Evoting/urls.py
@@ -6,13 +6,13 @@ app_name = 'Evoting'
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register_voter, name='register_voter'),  
-    path('vote/<str:faculty>/', views.vote, name='vote'),
+    path('vote_faculty/<str:faculty>/', views.vote, name='vote_faculty'),
     path('results/', views.results, name='results'),
     path('home/', views.home_view, name='home'),
     path('verify/<int:national_id>/', views.verify_voter, name='verify_voter'),
     path('voters/', views.voter_list, name='voter_list'),
     path('staff/dashboard/', views.staff_dashboard, name='staff_dashboard'),
     path('generate_pdf/<int:national_id>/', views.generate_pdf, name='generate_pdf'),
-    path('select_faculty/', views.select_faculty, name='select_faculty'),
+    path('vote/', views.select_faculty, name='select_faculty'),
     
 ]
