0d966a50c1dd69f2140925b1373e60c542b31ecb
Kim-254-de <abt5.09919.23@student.tharaka.ac.ke>
2025-07-19 12:02:28 +0300

    Add Changes

--- VotingSystem/settings.py
+++ VotingSystem/settings.py
@@ -33,7 +33,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False, cast=bool)
+DEBUG = config('DEBUG',default=True, cast=bool)
 
 ALLOWED_HOSTS =config('ALLOWED_HOSTS', cast=Csv())
 
