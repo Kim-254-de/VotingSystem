f9a3540ef80e6a12724eb738ca39e01be92ffa32
Kim-254-de <abt5.09919.23@student.tharaka.ac.ke>
2025-07-22 14:43:38 +0300

    Adding features for voter photos

--- VotingSystem/settings.py
+++ VotingSystem/settings.py
@@ -37,9 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 
 # For WhiteNoise
